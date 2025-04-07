d086b3cdc965627a5127f36a43f00d09e56c5b3d
QIZIR1 <riziqrizwan2@gmail.com>
2025-04-07 20:45:20 +0700

    menambahkan class <KuliahMahasiswa103022300119>

---
+++ tpmodul7_kelompok_7/KuliahMahasiswa103022300119.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul7_kelompok_7
+{
+    class KuliahMahasiswa103022300119
+    {
+        public List<Course> courses { get; set; }
+    }
+    public class Course
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+    }
+}
